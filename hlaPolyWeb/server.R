df63cc341eeed435e1ed7642d60eb37c4ee81900
lawlojohn <lawlojohn@gmail.com>
2019-10-29 11:52:24 -0700

    To select IMGT version

--- hlaPolyWeb/server.R
+++ hlaPolyWeb/server.R
@@ -1,67 +1,63 @@
-library(shiny)
+require(shiny)
 require("hlaPoly")
+require("DT")
 
-hlaData<-hlaDataInit()
 
 function(input, output) {
 
-  datasetInput <- eventReactive(input$go,{
-	locus = sub("\\*.*","",input$nucAllele)
-	allele2d = sub(":.*","",input$nucAllele)
-	if(is.na(hlaData$refAllele[allele2d])){
-       genAllele = as.character(hlaData$refAllele[locus])
-	}
-	else{
-       genAllele = as.character(hlaData$refAllele[allele2d])
+	hlaData <- eventReactive(input$versionInput,{
+		hlaDataInit(input$versionInput)
+	})
+	genAllele<-eventReactive(input$nucAllele,{
+		locus = sub("\\*.*","",input$nucAllele)
+		allele2d = sub(":.*","",input$nucAllele)
+		if(is.na(hlaData()$refAllele[allele2d])){
+			genAllele_local = as.character(hlaData()$refAllele[locus])
+		}
+		else{
+			genAllele_local = as.character(hlaData()$refAllele[allele2d])
+		}
+		genAllele_local
 
-	}
-    hlaGetAlign(hlaData, genAllele, input$nucAllele, input$conseq)
-  })
+	})
+  
+	datasetInput <- eventReactive(input$go,{
+    	hlaGetAlign(hlaData(), genAllele(), input$nucAllele, input$conseq)
+  	})
 
-  output$view <- renderDataTable({
-     hlaAlign = datasetInput()
-	mydata = hlaAlign2Novel(hlaAlign)
-	if(is.na(mydata)){
-		mydata
-	}
-	else{
-	levels(mydata$reftype)=c("17ws reference","closest allele","upstream unreferenced","downstream unreferenced","seq extension")
-	colnames(mydata)[8]="feature_number"
-	colnames(mydata)[1]="reference type"
-	mydata[,c(1,8,9,10,4,5,6,7,2,3)]
-	}
-  },
+	output$view <- renderDataTable({
+    	hlaAlign = datasetInput()
+		mydata = hlaAlign2Novel(hlaAlign)
+		if(is.na(mydata)){
+			mydata
+		}
+		else{
+			levels(mydata$reftype)=c("17ws reference","closest allele","upstream unreferenced","downstream unreferenced","seq extension")
+			colnames(mydata)[8]="feature_number"
+			colnames(mydata)[1]="reference type"
+			mydata[,c(1,8,9,10,4,5,6,7,2,3)]
+		}
+	},
 	options = list(
-  autoWidth = FALSE,
-	scrollX=TRUE,
-  columnDefs = list(list(width = '200px', targets =c(5,6) ))
-  ))
+		autoWidth = FALSE,
+		scrollX=TRUE,
+  		columnDefs = list(list(width = '200px', targets =c(5,6) ))
+  	))
 
-  output$text<- renderPrint({
-	hlaAlign = datasetInput()
-	cc=as.character(hlaAlign$triomsa)
-	for(i in seq(1, nchar(cc[1]), 100)){
-		print(i)
-		j=i+99
-		if(j>=nchar(cc[1])) j=nchar(cc[1])
-		cat(paste(substring(cc[1],i,j),names(cc[1]),sep="\t"),"\n")
-		cat(paste(substring(cc[2],i,j),names(cc[2]),sep="\t"),"\n")
-		cat(paste(substring(cc[3],i,j),names(cc[3]),sep="\t"),"\n")
-	}
-  })
-  output$genref<-renderText({
-	
-	locus = sub("\\*.*","",input$nucAllele)
-	allele2d = sub(":.*","",input$nucAllele)
-	if(is.na(hlaData$refAllele[allele2d])){
-       genAllele = as.character(hlaData$refAllele[locus])
-	}
-	else{
-       genAllele = as.character(hlaData$refAllele[allele2d])
-
-	}
-	genAllele
-
- })
+	output$text<- renderPrint({
+		hlaAlign = datasetInput()
+		cc=as.character(hlaAlign$triomsa)
+		for(i in seq(1, nchar(cc[1]), 100)){
+			print(i)
+			j=i+99
+			if(j>=nchar(cc[1])) j=nchar(cc[1])
+			cat(paste(substring(cc[1],i,j),names(cc[1]),sep="\t"),"\n")
+			cat(paste(substring(cc[2],i,j),names(cc[2]),sep="\t"),"\n")
+			cat(paste(substring(cc[3],i,j),names(cc[3]),sep="\t"),"\n")
+		}
+	})
+	output$genref<-renderText({
+		genAllele()
+	})
 	
 }
--- hlaPolyWeb/ui.R
+++ hlaPolyWeb/ui.R
@@ -1,4 +1,8 @@
-library(shiny)
+require(shiny)
+require("DT")
+
+imgtVersions=c("3.25.0","3.26.0","3.28.0","3.30.0","3.34.0","3.38.0")
+names(imgtVersions)=imgtVersions
 
 # Define UI for application that draws a histogram
 fluidPage(theme="bootstrap.min.css",
@@ -9,13 +13,17 @@ fluidPage(theme="bootstrap.min.css",
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
+     selectInput("versionInput",label="IMGT/HLA version",choices=imgtVersions,selected="3.25.0"), 
 	textInput("nucAllele", "Closest Allele:", "HLA-DPA1*01:03:01:01"),
-	actionButton("go", "Go"),
+  
+  strong("Reference Allele:"),
 #	textInput("subm", "sum matrix", "Gonnet"),
 	h3(textOutput("genref")),
-	h2(Sys.getenv('R_USER')),
-	h2(tempdir()),
-	h2("fff"),
+  	actionButton("go", "Go"),
+  br(),
+  br(),
+  strong("Nucleotide sequence:"),
+  br(),
 #	numericInput("sposition","Start Position:",0,min=0,max=10000),
 	tags$textarea(id="conseq",placeholder= "Consensus Seq:",rows=70, cols=50, "TACCGATAACTAACTGAGTAGTTAATATGGTCAGGCGCTATTCTGAGGATTTACATTTATTAACTCACTTTATTCTCACACATAGTCTTTGAGGTAGGTACTATTATTTTCACTATTTCACATGAGAGATACTTACATCTTTTTACATACACAGAGACTTTAAGCACTTTGATCAAGTTCCCACAGCTATGAAGTAGTAGGGCTAGCTTCCAATCCAGAAAGTCTGGATCCAAGACTGTTTATCCACTGTCCTATTCACCCTATTTTGTGAAGGAAAAGACCAAGTTCAAATTCTCCAGAGTCCATTGCCAAATAATGGAGTCAGATCTATATTTCTATACATAATTACAACACAGTGTGGTGGGTGCCTGTAACTACTTACTGTCTCTACTTGGACTCATTCCATGGCAATGTTCACACAAAAAATGCC")
     ),
